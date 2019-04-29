f76ecabc534f871900038c7a358adfc4b7782265
gongzhijie0518 <zhijie.gong@hand-china.com>
2019-04-29 11:04:34 +0800

    weblogic 测试

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,7 +1,13 @@
 package demo;
 
+
+
+
+//请通过Stream流的方式，将下面数组当中的元素添加（收集）到 List 集合当中
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("=======================");
+        int[] array = { 10, 20, 30, 40, 50};
+
+
     }
 }
